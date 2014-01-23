3e5e5378c84c629c5dbba375f08b74801593768e
tukiyo3 <tukiyo3@gmail.com>
2014-01-24 08:36:59 +0900

    ポートを複数追加できるようにした。  * オプションを変更しました。  * ポート番号の範囲チェック追加

--- ipscan.go
+++ ipscan.go
@@ -10,12 +10,13 @@ import (
 )
 
 var (
-	tcp_ports   = []int{22, 80, 23}
-	timeout     = 3 * time.Second
-	ip_start    = 1
-	ip_end      = 254
-	wg          sync.WaitGroup
-	target_host string
+	tcp_ports       = []int{22, 80, 23}
+	timeout         = 3 * time.Second
+	ip_start        = 1
+	ip_end          = 254
+	wg              sync.WaitGroup
+	target_host     string
+	supplement_port int
 )
 
 func analyze(target_host string) {
@@ -36,8 +37,9 @@ func analyze(target_host string) {
 
 func help() {
 	fmt.Println("check ports: tcp", tcp_ports)
-	println("usage: ipscan -h [hostname|ip] [-p port_number]")
-	println("       ipscan -i 192.168.11. [-p port_number]")
+	println("usage: ipscan [-o|--one] [hostname|ip] [-p|--port port_number]")
+	println("       ipscan [-m|--multi] 192.168.11. [-p|--port port_number]")
+	println("       ipscan -h")
 }
 
 func main() {
@@ -45,16 +47,25 @@ func main() {
 	if len(os.Args) < 3 || len(os.Args) == 4 {
 		help()
 		return
-	} else if len(os.Args) == 5 {
-		supplement_port, _ := strconv.Atoi(os.Args[4])
-		tcp_ports = append(tcp_ports, supplement_port)
+	} else if len(os.Args) >= 5 && (os.Args[3] == "-p" || os.Args[3] == "--port") {
+		for p := 4; p < len(os.Args); p++ {
+			supplement_port, _ = strconv.Atoi(os.Args[p])
+			if supplement_port < 1 || 65534 < supplement_port {
+				println("[quit] port_number between 1 and 65534. error:", os.Args[p])
+				return
+			}
+			tcp_ports = append(tcp_ports, supplement_port)
+		}
 	}
 
 	if os.Args[1] == "-h" {
+		help()
+		return
+	} else if os.Args[1] == "-o" || os.Args[1] == "--one" {
 		wg.Add(1)
 		target_host = os.Args[2]
 		analyze(target_host)
-	} else if os.Args[1] == "-i" {
+	} else if os.Args[1] == "-m" || os.Args[1] == "--multi" {
 		for ip := ip_start; ip <= ip_end; ip++ {
 			wg.Add(1)
 			target_host = os.Args[2] + strconv.Itoa(ip)
